75f0e971c1bdb39915ee58ac60b4842f403b6b31
Teerth <tr633364@dal.ca>
2024-07-01 19:11:12 -0300

    Implement sidebar for navigation on the left side of the page (on Mobile view as well).

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,13 +12,13 @@ const Sidebar = ({ user }: SiderbarProps) => {
     return (
         <section className='sidebar'>
             <nav className='flex flex-col gap-4'>
-                <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+                <Link href='/' className='mb-12 flex cursor-pointer items-center gap-2'>
                     <Image
                         src='icons/logo1.svg'
                         height={34}
                         width={34}
                         alt='CodeBank logo icon'
-                        className='size-[24px] max-xl:size-14'
+                        className='size-[26px] max-xl:size-14'
                     />
                     <h1 className='sidebar-logo'>
                         CodeBank
@@ -41,10 +41,18 @@ const Sidebar = ({ user }: SiderbarProps) => {
                                     })}
                                 />
                             </div>
+                            <p className={cn(
+                                'sidebar-label', { '!text-white': isActive })}>
+                                {item.label}
+                            </p>
                         </Link>
                     )
                 })}
+
+                USER
             </nav>
+
+            FOOTER
         </section>
     )
 }
