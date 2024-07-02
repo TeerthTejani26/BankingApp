a1f62847221a86f434394d4d253cd2e666ab1533
Teerth <tr633364@dal.ca>
2024-07-02 01:56:16 -0300

    Add another sidebar on the right side for showing more information on user's banks and accounts.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
-    const loggedIn = { firstName: 'Teerth' };
+    const loggedIn = { firstName: 'Teerth', lastName: 'Tejani', email: 'tejani@gmail.com'};
 
     return (
         <section className='home'>
@@ -22,7 +23,15 @@ const Home = () => {
                         totalCurrentBalance={1250.35}
                     />
                 </header>
+
+                RECENT TRANSACTIONS
             </div>
+
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[]}
+            />
         </section>
     )
 }
